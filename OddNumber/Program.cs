92656204555e1598ac55ecbab9b0f86410bfb6b2
KurtWeber <weberkrt@msn.com>
2016-09-09 09:25:32 -0400

    OddNumber Complete

---
+++ OddNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count;
+            for (count = 1; count <= 99; count++)
+            {
+                if (count % 2 == 1)
+                {
+                    Console.WriteLine(count);
+                }
+            }
+        }
+    }
+}
